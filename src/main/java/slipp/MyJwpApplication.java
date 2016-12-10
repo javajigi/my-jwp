021cf3e539fbbe394f090d71600c60486485c28e
javajigi <javajigi@gmail.com>
2016-12-10 16:34:56 +0900

    move test to src

--- src/main/java/slipp/MyJwpApplication.java
+++ src/main/java/slipp/MyJwpApplication.java
@@ -3,7 +3,6 @@ package slipp;
 import org.springframework.boot.SpringApplication;
 
 public class MyJwpApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyWebInitializer.class, args);
 	}
